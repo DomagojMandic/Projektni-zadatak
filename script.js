780e35b875f9428950bcec92348b6f4433c932a7
Domagoj Mandić <mandic.domagoj214@gmail.com>
2024-07-28 21:08:22 +0200

    Dobar dio responzivnosti + drag and scroll na dashboard.html - bit će dorađeno te će kasnije i CSS biti razložen na module radi preglednosti

--- script.js
+++ script.js
@@ -5,16 +5,25 @@
 const expandAsideBtn = document.getElementById("expandAsideEl");
 const asideBar = document.querySelector(".aside__bar");
 const contractAsideBtn = document.getElementById("closeAsideEl");
+const mainContainer = document.getElementById("mainContainer");
+const dashboardPodcasts = [
+  ...document.querySelectorAll("podcast__flex--anchor"),
+];
+
+/* Event listeneri za aside bar i funkcionalnost buttona za proširenje i smanjenje
+Aside bara */
 
 document.addEventListener("DOMContentLoaded", () => {
   /* Togglanje klasa vidljivosti */
   expandAsideBtn.addEventListener("click", () => {
     asideBar.classList.toggle("position__left--zero");
     contractAsideBtn.classList.toggle("hidden");
+    mainContainer.classList.toggle("background__content--visibility");
   });
 
   contractAsideBtn.addEventListener("click", () => {
     asideBar.classList.toggle("position__left--zero");
+    mainContainer.classList.toggle("background__content--visibility");
   });
 
   /* Short circuit u slučaju da se klikne unutar aside bara */
@@ -22,6 +31,8 @@ document.addEventListener("DOMContentLoaded", () => {
   document.addEventListener("click", (e) => {
     if (!asideBar.contains(e.target) && !expandAsideBtn.contains(e.target)) {
       asideBar.classList.remove("position__left--zero");
+      mainContainer.classList.remove("background__content--visibility");
+
       console.log(e);
     }
   });
@@ -29,3 +40,86 @@ document.addEventListener("DOMContentLoaded", () => {
 
 console.log(expandAsideBtn);
 console.log(asideBar);
+
+/* Carousel za 1280px za podcast anchore - funkcija primjenjiva na više stvari
+po ID-u */
+
+/* const dashboardPodcastCarousel = document.getElementById("podcastCarousel");
+const dashboardFeaturedCarousel = document.getElementById("featuredCarousel");
+const dashboardCategoriesCarousel =
+  document.getElementById("categoriesCarousel"); */
+
+const horizontalScrollArgs = [
+  "podcastCarousel",
+  "featuredCarousel",
+  "categoriesCarousel",
+];
+
+function enableHorizontalScroll(carouselId) {
+  const carousel = document.getElementById(carouselId);
+
+  if (!carousel) return;
+
+  if (window.matchMedia("(max-width: 1280px)").matches) {
+    const anchors = [...carousel.children];
+    anchors.forEach((anchor) => {
+      anchor.addEventListener("dragstart", (e) => e.preventDefault());
+    });
+    console.log(anchors);
+
+    let pressDown = false;
+    let startScrollX;
+    let scrollLeft;
+
+    carousel.addEventListener("mousedown", (e) => {
+      anchors.forEach((anchor) => {
+        anchor.classList.add("disable__pointers");
+      });
+      pressDown = true;
+      startScrollX = e.pageX - carousel.offsetLeft;
+      scrollLeft = carousel.scrollLeft;
+    });
+
+    carousel.addEventListener("mouseleave", () => {
+      anchors.forEach((anchor) => {
+        anchor.classList.remove("disable__pointers");
+      });
+      pressDown = false;
+    });
+
+    carousel.addEventListener("mouseup", () => {
+      anchors.forEach((anchor) => {
+        anchor.classList.remove("disable__pointers");
+      });
+      pressDown = false;
+    });
+
+    carousel.addEventListener("mousemove", (e) => {
+      if (!pressDown) return;
+      e.preventDefault();
+      const x = e.pageX - carousel.offsetLeft;
+      const move = x - startScrollX;
+      carousel.scrollLeft = scrollLeft - move;
+    });
+  }
+}
+
+horizontalScrollArgs.forEach(enableHorizontalScroll);
+
+/* Kontroliranje pozicije offline i online elemenata anchora za podcaste  - 
+
+Media Query 992px*/
+
+const listaDashboardPodcasts = [...dashboardPodcasts];
+
+listaDashboardPodcasts.forEach((podcastsListItem) => {
+  const widthPodcastsItem = window.getComputedStyle(podcastsListItem).width;
+
+  if (parseFloat(widthPodcastsItem) > 308) {
+    const onlineContainer = podcastsListItem.querySelector(".flex__row--start");
+    console.log(onlineContainer);
+
+    onlineContainer.style.justifySelf = "flex-end";
+    onlineContainer.style.marginRight = "10px";
+  }
+});
